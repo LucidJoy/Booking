e11ec244d379de631eb9bcab067e25e406e8e48d
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-04-29 19:06:40 +0530

    Footer design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Featured from "../../components/featured/Featured";
+import Footer from "../../components/footer/Footer";
 import GuestHomes from "../../components/guestHomes/GuestHomes";
 import Header from "../../components/header/Header";
 import MailList from "../../components/mailList/MailList";
@@ -18,6 +19,7 @@ const Home = () => {
         <h2 className='homeTitle'>Homes guests love</h2>
         <GuestHomes />
         <MailList />
+        <Footer />
       </div>
     </div>
   );
