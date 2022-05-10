5b3841c4f7aaab14f9f4c144d48825ea356df03d
LucidJoy <jyotirmoykarmakar02@gmail.com>
2022-05-10 20:43:11 +0530

    JWT based authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,9 @@
 import express from "express";
+import { login, register } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("AUTH PAGE");
-});
-
-router.get("/register", (req, res) => {
-  res.send("AUTHREG PAGE");
-});
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
